8e08f0e59cd3aa3c3640005080c56975d5a5e8af
Hubert Zajączkowski <01121470@pw.edu.pl>
2023-04-16 16:14:15 +0200

    Header and Footer added

---
+++ src/components/Header.jsx
@@ -0,0 +1,163 @@
+// Import section
+import React, { useRef } from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
+import "../styles/header.css";
+
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/cars",
+    display: "Cars",
+  },
+
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
+
+const Header = () => {
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => menuRef.current.classList.toggle("menu__active");
+
+  return (
+    <header className="header">
+      {/* ============ header top ============ */}
+      <div className="header__top">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="header__top__left">
+                <span>Potrzebujesz pomocy?</span>
+                <span className="header__top__help">
+                  <i className="ri-phone-fill"></i> +48 987-654-321
+                </span>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="6">
+              <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
+                <Link to="#" className=" d-flex align-items-center gap-1">
+                  <i className="ri-login-circle-line"></i> Zaloguj się
+                </Link>
+
+                <Link to="#" className=" d-flex align-items-center gap-1">
+                  <i className="ri-user-line"></i> Załóż konto
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      {/* =============== header middle =========== */}
+      <div className="header__middle">
+        <Container>
+          <Row>
+            <Col lg="4" md="3" sm="4">
+              <div className="logo">
+                <h1>
+                  <Link to="/home" className=" d-flex align-items-center gap-2">
+                    <i className="ri-car-line"></i>
+                    <span>
+                      Wypożyczalnia <br/> Carently
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location d-flex align-items-center gap-2">
+                <span>
+                  <i className="ri-earth-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Polska</h4>
+                  <h6>Warszawa, Kraków</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location d-flex align-items-center gap-2">
+                <span>
+                  <i className="ri-time-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Poniedziałek-Piątek</h4>
+                  <h6>6 - 22</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col
+              lg="2"
+              md="3"
+              sm="0"
+              className=" d-flex align-items-center justify-content-end "
+            >
+              <button className="header__btn btn ">
+                <Link to="/contact">
+                  <i className="ri-phone-line"></i> Skontaktuj się
+                </Link>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      {/* ========== main navigation =========== */}
+
+      <div className="main__navbar">
+        <Container>
+          <div className="navigation__wrapper d-flex align-items-center justify-content-between">
+            <span className="mobile__menu">
+              <i className="ri-menu-line" onClick={toggleMenu}></i>
+            </span>
+
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+              <div className="menu">
+                {navLinks.map((item, index) => (
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive ? "nav__active nav__item" : "nav__item"
+                    }
+                    key={index}
+                  >
+                    {item.display}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+
+            <div className="nav__right">
+              <div className="search__box">
+                <input type="text" placeholder="Search" />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 
 // Root point
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
+const element = (
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>
 );
+
+root.render(element);
+
